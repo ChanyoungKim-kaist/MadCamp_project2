9bdc62957118b46448f8930cf068308cd0537444
kimchanyoung <itnoj15@naver.com>
2021-07-09 21:27:06 +0900

    UI and kakao login

---
+++ src/main/java/com/example/loginui_kakao/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.loginui_kakao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.kakao.auth.ISession;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.util.exception.KakaoException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ISessionCallback mSessionCallback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mSessionCallback = new ISessionCallback() {
+            @Override
+            public void onSessionOpened() { //로그인 요청
+                UserManagement.getInstance().me(new MeV2ResponseCallback() {
+                    @Override
+                    public void onFailure(ErrorResult errorResult) { //로그인 실패
+                        Toast.makeText(MainActivity.this, "로그인 도중 오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onSessionClosed(ErrorResult errorResult) {
+                        Toast.makeText(MainActivity.this, "세션이 닫혔습니다. 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onSuccess(MeV2Response result) { //로그인 성공
+                        /*
+                        Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                        intent.putExtra("name", result.getKakaoAccount().getProfile().getNickname());
+                        intent.putExtra("profileImg", result.getKakaoAccount().getProfile().getProfileImageUrl());
+                        intent.putExtra("email", result.getKakaoAccount().getEmail());
+                        startActivity(intent);*/
+
+                        Toast.makeText(MainActivity.this, "환영합니다!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onSessionOpenFailed(KakaoException exception) {
+
+            }
+        };
+        Session.getCurrentSession().addCallback(mSessionCallback);
+        Session.getCurrentSession().checkAndImplicitOpen();
+    }
+}
